2162e76092ab1e8ac7fac465499a2b12396e9125
João Sarmento <joaovicentesarm@gmail.com>
2022-02-08 14:04:19 -0300

    Páginas e Menus Prontos

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
 let NomeUsuário;
 
+
+// Função para validar o nome colocado na tela de entrada
 function ValidarNome(){
     NomeUsuário = document.querySelector('input.Nome').value;
     if(NomeUsuário!==''){
@@ -7,6 +9,12 @@ function ValidarNome(){
     }
 }
 
+// Função para acionar o menu ao clicar no ícone superior à direita
 function Menu(){
     document.querySelector('.Menu').classList.remove('escondido');
+}
+
+// Função para voltar do menu à página do chat ao clicar no fundo
+function Voltar(){
+    document.querySelector('.Menu').classList.add('escondido');
 }
