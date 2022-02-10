2926872a95b5b8c8c4a93d41154004287e491e47
João Sarmento <joaovicentesarm@gmail.com>
2022-02-10 16:46:11 -0300

    Checando nome de usuário/Mensagens chegando e sendo enviadas

--- script/script.js
+++ script/script.js
@@ -1,17 +1,191 @@
-let NomeUsuário;
+let mensagens = [];                         // Array que segura todas as mensagens da API
+let contador = 99;                          // Contador que diz qual a primeira mensagem que aparecerá na tela
+let TipoDeMensagem = 'message';             // Tipo de mensagem inicial selecionada
+let Remetente = 'todos';                    // Remetente inicial selecionado
+let UltimaMensagem;                         // Guarda a ultima mensagem para poder colocar as novas mensagens na tela
+let chat = document.querySelector('main');  // Pega o main do html para colocar mensagens
+let contadorScroll = 0;                     // Contador para manter as mensagens scrolladas
+let NomeUsuário = [];                       // Nome que o usuário escolhe
+
+
+
+//Iniciar o chat
+function IniciarChat(){
+    TrazerMensagensServidor();
+    setInterval(TrazerMensagensServidor,3000);
+    setInterval(MostrarMensagens,500);
+}
+
+//Trazer mensagens do servidor
+function TrazerMensagensServidor(){
+    const promessa = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
+    promessa.then(CarregarMensagens);
+}
+
+
+//Colocar as mensagens dentro do array na página
+function CarregarMensagens(mensagem) {
+    mensagens = mensagem.data;
+    MostrarMensagens();
+}
+
+
+//Funçao para mostrar as mensagens na tela
+function MostrarMensagens(){
+    if(contador<mensagens.length){
+        SelecionarMensagem();
+        UltimaMensagem = mensagens[contador];
+        contador++;
+    } else{
+        if(mensagens[contador-1] !== UltimaMensagem){
+            contador = procurarIndice(UltimaMensagem.name,UltimaMensagem.text);
+        }
+    }
+}
+
+
+//Procurar ultima mensagem enviada no array anterior
+function procurarIndice(nome, mensagem){
+    var cont=0;
+    let indice;
+    for(let i=0;i<mensagens.length;i++){
+       if(mensagens[i].name==nome&&mensagens[i].text==mensagem){
+          indice=i;
+       }
+    }
+    return indice+1;
+ }
+
+
+//Selecionar qual o tipo de mensagem
+function SelecionarMensagem(){
+    if(mensagens[contador].type === 'status'){
+        mensagemDeStatus();
+    }else{
+        MensagemDeTexto();
+    }
+}
+
+//Formato da mensagem de status
+function mensagemDeStatus(){
+    chat.innerHTML += `<p class="${mensagens[contador].type}"><span class="s${contadorScroll}">(${mensagens[contador].time}) </span>  <strong>${mensagens[contador].from}</strong> ${mensagens[contador].text}</p>`;
+    const tempo = chat.querySelector(`.s${contadorScroll}`);
+    tempo.scrollIntoView();
+    contadorScroll++;
+}
+
+
+//Formato da mensagem de texto
+function MensagemDeTexto(){
+    chat.innerHTML += `<p class="${mensagens[contador].type}"><span class="s${contadorScroll}">(${mensagens[contador].time})</span>  <strong>${mensagens[contador].from}</strong> para <strong>${mensagens[contador].to}</strong>:  ${mensagens[contador].text}</p>`;
+    const tempo = chat.querySelector(`.s${contadorScroll}`);
+    tempo.scrollIntoView();
+    contadorScroll++;
+}
 
 
 // Função para validar o nome colocado na tela de entrada
 function ValidarNome(){
-    NomeUsuário = document.querySelector('input.Nome').value;
+    const Nome = document.querySelector('input.Nome').value;
+
+    NomeUsuário = {
+        name: Nome
+    }
+
     if(NomeUsuário!==''){
-        document.querySelector('.Tela-Entrada').classList.add('escondido');
+        PostNome();
     }
 }
+
+
+//Função para dar post no nome do usuário
+function PostNome(){
+    let request = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants',NomeUsuário);
+    request.then(NomeDisponivel);
+    request.catch(NomeIndisponivel);
+}
+
+
+//Função acionada se o nome está disponivel
+function NomeDisponivel(mensagem){
+    if(mensagem.status==200){
+        document.querySelector('.Tela-Entrada').classList.add('escondido');
+        IniciarChat();
+        setInterval(MaintainConection,5000);
+    } else{
+        alert('Algo deu errado!');
+    }
+}
+
+//Função acionada se o nome ja existir  no chat
+function NomeIndisponivel(erro){
+    if(erro.response.status == 400){
+        alert('Esse nome já foi escolhido por outra pessoa!! Escolha outro nome');
+    } else{
+        alert('Erro!(Não reconhecido)')
+    }
+}
+
+//Função para manter conexão, enviando o nome de usuário a cada 5 segundos
+function MaintainConection(){
+    let request = axios.post('https://mock-api.driven.com.br/api/v4/uol/status',NomeUsuário);
+}
+
+
+// Função para enviar mensagem para servidor
+function MandarMensagem(){
+    const MensagemNova = document.querySelector('.MensagemNova').value;
+    if(MensagemNova!==''){
+
+        const Mensagem = {
+            from: NomeUsuário.name,
+            to: Remetente,
+            text: MensagemNova,
+            type: TipoDeMensagem // ou "private_message" para o bônus
+        }
+    
+        document.querySelector('.MensagemNova').value = '';
+
+        const request = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages', Mensagem);
+        request.then(ConfirmaMensagem);
+        request.catch(ErroMensagem);
+
+    }
+}
+
+
+//Função que diz que mensagem deu certo
+function ConfirmaMensagem(){
+    TrazerMensagensServidor();
+}
+
+
+//Função para caso a mensagem de erro de envio
+function ErroMensagem(){
+    alert('Erro ao enviar mensagem!');
+    window.location.reload();
+}
+
+
+
+let participantes = [];
 
 // Função para acionar o menu ao clicar no ícone superior à direita
 function Menu(){
     document.querySelector('.Menu').classList.remove('escondido');
+    request = axios.get('https://mock-api.driven.com.br/api/v4/uol/participants');
+    request.then(Imprimirparticipantes);
+}
+
+
+//Função para imprimir os participantes online na tela de menu
+function Imprimirparticipantes(dados){
+    const online = document.querySelector('nav .PessoasOnline');
+    online.innerHTML = '';
+    participantes = dados.data;
+    for(let i=0;i<participantes.length;i++){
+    online.innerHTML += `<p>${participantes[i].name}</p>`
+    }
 }
 
 // Função para voltar do menu à página do chat ao clicar no fundo
